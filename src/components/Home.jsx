5dee7a8822b539840c04ff6b61cf89526bc9c1ab
AvikNayak <110925067+AvikNayak22@users.noreply.github.com>
2023-07-04 18:28:24 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="w-full h-screen bg-[#060918] flex justify-center items-center"
+    >
+      {/* container */}
+      <div className="max-w-[2000px] max-auto px-6 flex flex-col justify-center items-center h-full">
+        <h1 className="text-2xl sm:text-5xl font-bold text-[#FFFEF9]">
+          Hello,my name is Avik Nayak
+        </h1>
+        <h3 className="text-xl sm:text-4xl text-[#585F78] py-4 max-w-[700px]">
+          I do front-end development.
+        </h3>
+        <div>
+          <Link to="work" smooth={true} duration={500}>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#3a75c2] hover:border-[#3a75c2]">
+              View Projects
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Work.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import MusicPlayer from "../assets/projects/MusicPlayer.jpg";
+import Country from "../assets/projects/Country.jpg";
+import VisualSort from "../assets/projects/VisualSort.jpg";
+
+const Work = () => {
+  return (
+    <div name="work" className="w-full sm:h-screen text-gray-300 bg-[#060918]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-[#3a75c2]">
+            Projects
+          </p>
+          <p className="py-4">//Check out some of my recent Projects</p>
+        </div>
+
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${Country})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Country Info Finder
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://an-country-info-app.netlify.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/AvikNayak22/Air-Pollution-Indicator"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${MusicPlayer})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Mini Music Player
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://aviknayak22.github.io/Minimalistic-music-player/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/AvikNayak22/Minimalistic-music-player"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${VisualSort})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Bubble Sort Visualizer
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://aviknayak22.github.io/Sorting-Visualizer-V1/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/AvikNayak22/Sorting-Visualizer-V1"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
